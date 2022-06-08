0d60d132ee86c08b1db851427459a5c7e2fdac9b
Tit Hardwood <blnaxblachbl@gmail.com>
2022-06-08 11:39:41 +0900

    fixes

---
+++ containers/Home.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Container = styled.div``
+
+const HomeContainer = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default HomeContainer
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import HomeContainer from "../containers/Home"
+
+const Home = () => {
+    return (
+        <>
+            <HomeContainer />
+        </>
+    )
+}
+
+export default Home
